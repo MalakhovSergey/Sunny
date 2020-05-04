18fa4dc437532a3b02d1c5b9492dbf0899fc46cc
Sergey Malakhov <Kronkadet2014@icloud.com>
2020-05-04 16:11:03 +0300

    Initial commit

---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Sergey Malakhov on 04/05/2020.
+//  Copyright © 2020 Sergey Malakhov. All rights reserved.
+//
+
+
+import Foundation
+
+let apiKey = "9da95827e359a2b6149d90cda1308561"
